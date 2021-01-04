25cea5b46237fe71da2f8ea7f2fe7645f07961a5
Aaron Kujawa <askujawa@gmail.com>
2021-01-04 13:14:28 +0000

    Added requirements.txt

--- install_requirements.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export HOME=/workspace/home/packages/
-export PATH=$HOME/.local/bin:$PATH
-pip install git+https://github.com/Project-MONAI/MONAI.git@0b415f09db82a39f1df5bafd9b8ff67f9f3f677c
-pip install nibabel
-pip install natsort
-pip install tqdm --upgrade
